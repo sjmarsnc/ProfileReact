067f339b6976bafdaae5653e35134ad660f808e3
sjmars <sjmarsnc@gmail.com>
2020-04-02 23:07:28 -0400

    back out changes to deploy on github

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Portfolio from "./pages/Portfolio";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import NotFound404 from "./components/NotFound404";
+// import NotFound404 from "./components/NotFound404";
 // import "./App.css";
 
 function App() {
   return (
-    <Router basename={"/ProfileReact"}>
+    <Router>
       <Switch>
         <Route exact path={["/", "/Portfolio"]} component={Portfolio} />
         <Route exact path="/about" component={About} />
         <Route exact path="/contact" component={Contact} />
-        <Route component={NotFound404} />
+        {/* <Route component={NotFound404} /> */}
       </Switch>
     </Router>
   );
