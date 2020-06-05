35c02a5ed10766d7c7198ad8b982fadf33ec4675
sjmars <sjmarsnc@gmail.com>
2020-06-05 16:04:16 -0400

    add github icon to each portfolio project card with link to repo

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,6 +3,12 @@ import Skill from "./Skill";
 import "./card.css";
 
 function Project(props) {
+
+  const style = {
+    ghicon: {
+      color: "white"
+    }
+  }
   // console.log("PROPS in Card: ", props);
   return (
     <div
@@ -17,7 +23,13 @@ function Project(props) {
           src={props.img}
           alt="related to app"
         />
-        <div className="card-title projLabelBox">{props.name}</div>
+        <div className="card-title projLabelBox">{props.name}
+          <div class="ghrepo">
+            <a href={props.githubLink}>
+              <i className="fa fa-github" style={style.ghicon} />
+            </a>
+          </div>
+        </div>
         <div className="justify-content-around">
           {props.skills.map((skill, index) => (
             <Skill key={index} skill={skill} />
